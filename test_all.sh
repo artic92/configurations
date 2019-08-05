734f493a06a8c089e574b3c3535c528d9f5dc959
Antonio Riccio <antonio.riccio.27@gmail.com>
2019-08-05 11:57:17 +0100

    issue #79: library: create unit test

---
+++ test_all.sh
@@ -0,0 +1,215 @@
+#!/bin/bash
+
+# if set, the functions-under-test are actually called inside each test_* function
+SELF_CONTAINED_TESTS=0
+
+test_passed=0
+test_failed=0
+
+function test_setup_git()
+{
+    source "$HOME"/configurations/library.sh
+    local file_to_test="$HOME"/.gitconfig
+
+    $ECHO -n "test setup_git..."
+
+    if [[ $SELF_CONTAINED_TESTS -eq 1 ]]; then
+        setup_git &> /dev/null
+    fi
+
+    if [[ ! -L $file_to_test ]]; then
+        $ECHO "FAILED"
+        ((test_failed++))
+    elif [[ ! -s $file_to_test ]]; then
+        $ECHO "FAILED"
+        ((test_failed++))
+    else
+        $ECHO "PASSED"
+        ((++test_passed))
+    fi
+}
+
+function test_setup_vim()
+{
+    source "$HOME"/configurations/library.sh
+    local file_to_test="$HOME"/.vimrc
+
+    $ECHO -n "test setup_vim..."
+
+    if [[ $SELF_CONTAINED_TESTS -eq 1 ]]; then
+        setup_vim &> /dev/null
+    fi
+
+    if [[ ! -L $file_to_test ]]; then
+        $ECHO "FAILED"
+        ((test_failed++))
+    elif [[ ! -s $file_to_test ]]; then
+        $ECHO "FAILED"
+        ((test_failed++))
+    else
+        $ECHO "PASSED"
+        ((++test_passed))
+    fi
+}
+
+function test_setup_bash
+{
+    local bash_link_name=bashrc
+
+    source "$HOME"/configurations/library.sh
+
+    $ECHO -n "test setup_bash..."
+
+    if [[ "$OSTYPE" == "darwin"* ]]
+    then
+        bash_link_name=bash_profile
+    fi
+
+    if [[ $SELF_CONTAINED_TESTS -eq 1 ]]; then
+        setup_bash &> /dev/null
+    fi
+
+    if [[ -a $HOME/".$bash_link_name" ]]
+    then
+        $ECHO "PASSED"
+        ((++test_passed))
+    else
+        $ECHO "FAILED"
+        ((++test_failed))
+    fi
+}
+
+function test_setup_ssh()
+{
+    source "$HOME"/configurations/library.sh
+    local file_to_test="$SSH_DIR"/config
+
+    $ECHO -n "test setup_ssh..."
+
+    if [[ $SELF_CONTAINED_TESTS -eq 1 ]]; then
+        setup_ssh &> /dev/null
+    fi
+
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        if [[ ! -L $file_to_test ]]; then
+            $ECHO "FAILED"
+            ((test_failed++))
+        elif [[ ! -s $file_to_test ]]; then
+            $ECHO "FAILED"
+            ((test_failed++))
+        else
+            $ECHO "PASSED"
+            ((++test_passed))
+        fi
+    else
+        $ECHO "PASSED"
+        ((++test_passed))
+    fi
+}
+
+function test_setup_gdb()
+{
+    source "$HOME"/configurations/library.sh
+    local file_to_test="$HOME"/.gdbinit
+
+    $ECHO -n "test setup_gdb..."
+
+    if [[ $SELF_CONTAINED_TESTS -eq 1 ]]; then
+        setup_gdb &> /dev/null
+    fi
+
+    if [[ ! -L $file_to_test ]]; then
+        $ECHO "FAILED"
+        ((test_failed++))
+    elif [[ ! -s $file_to_test ]]; then
+        $ECHO "FAILED"
+        ((test_failed++))
+    else
+        $ECHO "PASSED"
+        ((++test_passed))
+    fi
+}
+
+function test_setup_terminator()
+{
+    source "$HOME"/configurations/library.sh
+    local file_to_test="$HOME"/.config/terminator/config
+
+    $ECHO -n "test setup_terminator..."
+
+    if [[ $SELF_CONTAINED_TESTS -eq 1 ]]; then
+        setup_terminator &> /dev/null
+    fi
+
+    if [[ ! -L $file_to_test ]]; then
+        $ECHO "FAILED"
+        ((test_failed++))
+    elif [[ ! -s $file_to_test ]]; then
+        $ECHO "FAILED"
+        ((test_failed++))
+    else
+        $ECHO "PASSED"
+        ((++test_passed))
+    fi
+}
+
+function test_setup_filezilla
+{
+    source "$HOME"/configurations/library.sh
+
+    $ECHO -n "test setup_filezilla..."
+
+    if [[ $SELF_CONTAINED_TESTS -eq 1 ]]; then
+        setup_filezilla &> /dev/null
+    fi
+
+    if [[ -a $HOME/.config/filezilla/filezilla.xml && -a $HOME/.config/filezilla/sitemanager.xml ]]
+    then
+        $ECHO "PASSED"
+        ((test_passed++))
+    else
+        $ECHO "FAILED"
+        ((test_failed++))
+    fi
+}
+
+function test_setup_go()
+{
+    source "$HOME"/configurations/library.sh
+    local file_to_test="$HOME"/.go/
+
+    $ECHO -n "test setup_go..."
+
+    if [[ $SELF_CONTAINED_TESTS -eq 1 ]]; then
+        setup_go &> /dev/null
+    fi
+
+    if [[ ! -d $file_to_test ]]; then
+        $ECHO "FAILED"
+        ((test_failed++))
+    else
+        $ECHO "PASSED"
+        ((++test_passed))
+    fi
+}
+
+###################################################
+#  MAIN script starts here
+###################################################
+
+if [[ $SELF_CONTAINED_TESTS -eq 1 ]]; then
+ echo -e "tests in stand-alone mode...\n"
+fi
+
+test_setup_git
+test_setup_vim
+test_setup_bash
+test_setup_gdb
+test_setup_ssh
+test_setup_terminator
+test_setup_filezilla
+test_setup_go
+
+echo -e "\nSUMMARY"
+echo -e "\ttests passed: $test_passed"
+echo -e "\ttests failed: $test_failed"
