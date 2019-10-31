64c0a153bf4b0e5916b54086e11653c857c57668
Antonio Riccio <antonio.riccio.27@gmail.com>
2019-10-31 15:35:24 +0000

    library: code refactoring

--- library.sh
+++ library.sh
@@ -1,89 +1,127 @@
 #!/bin/bash
 
-#   -------------------------------
-#   VARIABLES CONFIGURATION
-#   -------------------------------
-
-GIT_USER=artic92
-GIT_REPO_HTTPS=https://github.com/$GIT_USER
-GIT_REPO_SSH=git@github.com:$GIT_USER
-
+###################################################
+#  commands macro
+###################################################
 ECHO='echo -e'
 CP='cp -r'
 LN='ln -sf'
 RM='rm -rf'
 CD=cd
 MKDIR=mkdir
+CHMOD=chmod
 GIT=git
 SUDO=sudo
-PKT_MNGR=apt-get
-PKT_MNGR_CMD=install
-PKT_MGR_OPTS=-y
-INSTALL="$SUDO $PKT_MNGR $PKT_MNGR_CMD $PKT_MGR_OPTS"
 
-HOME=~
-SSH_DIR=$HOME/.ssh
-CONFIGURATIONS_DIR=$HOME/configurations
-HOWTOS_DIR=$HOME/howtos
-BASHSCRIPTING_DIR=$HOME/bash_scripting
+###################################################
+#  useful paths
+###################################################
+SSH_DIR="$HOME"/.ssh
+CONFIGURATIONS_DIR="$HOME"/configurations
 
-DEBUG=true
+###################################################
+#  error definitions
+###################################################
 
-#   -------------------------------
-#   GIT CONFIGURATION
-#   -------------------------------
 
-# configure git global info
-git_set_global_info()
+###################################################
+#  service functions library
+###################################################
+
+function git_set_global_info()
 {
     $GIT config --global user.name "Antonio Riccio"
     $GIT config --global user.email "antonio.riccio.27@gmail.com"
 }
 
-# get the git global config
-git_get_global_info()
+function git_get_global_info()
 {
     $ECHO "[GIT] global username: $($GIT config user.name)"
     $ECHO "[GIT] global email: $($GIT config user.email)"
 }
 
-#   -------------------------------
-#   SERVICE FUNCTIONS
-#   -------------------------------
-
-setup_git()
+function setup_git()
 {
-    $LN $HOME/configurations/git/.gitconfig $HOME/.gitconfig
+    $LN "$CONFIGURATIONS_DIR"/git/.gitconfig "$HOME"/.gitconfig
 }
 
-# set the current user as sudo
+function setup_vim()
+{
+    $LN "$CONFIGURATIONS_DIR"/vim/.vimrc "$HOME"/.vimrc
+}
 
-# install bashrc
 function setup_bash()
 {
     local bash_link_name=bashrc
 
-    $LN $HOME/configurations/bash/.bash_aliases $HOME/.bash_aliases
+    $LN "$CONFIGURATIONS_DIR"/bash/.bash_aliases "$HOME"/.bash_aliases
 
-    if [[ "$OSTYPE" == "darwin"* ]]
-    then
+    if [[ "$OSTYPE" == "darwin"* ]]; then
         bash_link_name=bash_profile
     fi
 
-    $LN $HOME/configurations/bash/.bashrc $HOME/".$bash_link_name"
-    source $HOME/".$bash_link_name"
+    $LN "$CONFIGURATIONS_DIR"/bash/.bashrc "$HOME/.$bash_link_name"
+    source "$HOME/.$bash_link_name"
 }
 
-# install vim configuration
-setup_vim()
+function setup_ssh()
 {
-    $LN $HOME/configurations/vim/.vimrc $HOME/.vimrc
+    if [[ ! -d "$SSH_DIR" ]]; then
+        $MKDIR "$SSH_DIR"
+    fi
+
+    $LN "$CONFIGURATIONS_DIR"/ssh/known_hosts "$SSH_DIR"/known_hosts
+
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        $LN "$CONFIGURATIONS_DIR"/ssh/macos/config "$SSH_DIR"/config
+    fi
 }
 
-setup_terminator()
+# ------------------------------------------------------------------- #
+# setup_ssh_key()                                                     #
+# Gives the right permissions to the SSH keys                         #
+# Parameters: -                                                       #
+# NOTE: it assumes the keys are already installed in $HOME/.ssh       #
+# ------------------------------------------------------------------- #
+function setup_ssh_keys()
 {
-    $MKDIR $HOME/.config/terminator
-    $LN $HOME/configurations/terminator/config $HOME/.config/terminator/
+    local ssh_key_name=id_rsa
+
+    $CHMOD 700 "$SSH_DIR"
+    $CHMOD 644 "$SSH_DIR"/$ssh_key_name.pub
+    $CHMOD 600 "$SSH_DIR"/$ssh_key_name
+}
+
+function setup_gdb()
+{
+    $LN "$CONFIGURATIONS_DIR"/gdb/.gdbinit "$HOME"/.gdbinit
+}
+
+function setup_terminator()
+{
+    local terminator_target_dir="$HOME"/.config/terminator
+
+    if [[ ! -d $terminator_target_dir ]]; then
+        $MKDIR "$terminator_target_dir"
+    fi
+    $LN "$CONFIGURATIONS_DIR"/terminator/config "$terminator_target_dir"/
+}
+
+function setup_filezilla()
+{
+    local filezilla_targer_dir="$HOME"/.config/filezilla
+
+    if [[ ! -d $filezilla_targer_dir ]]; then
+        $MKDIR "$filezilla_targer_dir"
+    fi
+
+    $LN "$CONFIGURATIONS_DIR"/filezilla/filezilla.xml "$filezilla_targer_dir"/filezilla.xml
+    $LN "$CONFIGURATIONS_DIR"/filezilla/sitemanager.xml "$filezilla_targer_dir"/sitemanager.xml
+}
+
+function setup_go()
+{
+    $LN "$CONFIGURATIONS_DIR"/go/.go "$HOME"
 }
 
 function setup_vscode()
@@ -101,83 +139,33 @@ function setup_vscode()
     $ECHO
 }
 
-function setup_ssh()
+function docker_install_useful_containers()
 {
-    if [[ ! -d $SSH_DIR ]]
-    then
-        $MKDIR $SSH_DIR
-    fi
-
-    $LN $HOME/configurations/ssh/known_hosts $SSH_DIR/known_hosts
-
-    if [[ "$OSTYPE" == "darwin"* ]]
-    then
-        # Mac OSX
-        $LN $HOME/configurations/ssh/macos/config $SSH_DIR/config
-    fi
+    docker pull jgiovaresco/texmaker
+    docker pull linuxserver/transmission
+    docker pull jerivas/popcorntime
+    docker pull jess/tor-browser
 }
 
-setup_gdb()
-{
-    $LN $HOME/configurations/gdb/.gdbinit $HOME/.gdbinit
-}
-
-function setup_go()
-{
-    $LN $HOME/configurations/go/.go $HOME/
-}
-
-function setup_filezilla()
-{
-    local filezilla_dir="$HOME/.config/filezilla"
-
-    if [[ ! -d $filezilla_dir ]]
-    then
-        $MKDIR "$filezilla_dir"
-    fi
-
-    $LN $CONFIGURATIONS_DIR/filezilla/filezilla.xml $HOME/.config/filezilla/filezilla.xml
-    $LN $CONFIGURATIONS_DIR/filezilla/sitemanager.xml $HOME/.config/filezilla/sitemanager.xml
-}
-
-# install ssh keys
-setup_ssh_keys()
-{
-    #COPY MANUALLY THE SSH KEYS!
-
-    $CD $HOME/.ssh
-    chmod 700 .ssh
-    chmod 644 *.pub
-    chmod 600 id_rsa
-}
-
-docker_install_useful_containers()
-{
-  docker pull jgiovaresco/texmaker
-  docker pull linuxserver/transmission
-  docker pull jerivas/popcorntime
-  docker pull jess/tor-browser
-}
-
-setup_dropbox_autostart()
+function setup_dropbox_autostart()
 {
     dropbox autostart y
 }
 
-setup_minidlna()
+function setup_minidlna()
 {
-    install -Dm644 /etc/minidlna.conf $HOME/.config/minidlna/minidlna.conf
-    $LN $HOME/configurations/minidlna/minidlna.conf $HOME/.config/minidlna/minidlna.conf
+    install -Dm644 /etc/minidlna.conf "$HOME"/.config/minidlna/minidlna.conf
+    $LN "$CONFIGURATIONS_DIR"/minidlna/minidlna.conf "$HOME"/.config/minidlna/minidlna.conf
 
-    mkdir $HOME/.config/minidlna/cache
-    touch $HOME/.config/minidlna/minidlna.pid
+    $MKDIR "$HOME"/.config/minidlna/cache
+    touch "$HOME"/.config/minidlna/minidlna.pid
 
     $ECHO "\n\nCheck the general_notes regarding managing inodes\n\n"
 }
 
-start_minidlna_as_local_user()
+function start_minidlna_as_local_user()
 {
-    minidlnad -f $HOME/.config/minidlna/minidlna.conf -P $HOME/.config/minidlna/minidlna.pid
+    minidlnad -f "$HOME"/.config/minidlna/minidlna.conf -P "$HOME"/.config/minidlna/minidlna.pid
     #To autostart it at login, add the previous line to ~/.bash_profile
 }
 
